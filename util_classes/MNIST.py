5f23c984c22216fb15421d1e60480e95633fefa1
= <walker.richard.james@gmail.com>
2021-11-02 14:13:31 +0000

    MNIST Classes added CNN and MLP

---
+++ util_classes/MNIST.py
@@ -0,0 +1,241 @@
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+from numpngw import write_png
+from datetime import datetime
+import pickle
+from sklearn.metrics import confusion_matrix
+
+class MNISTBase():
+    def __init__(self, n="numbers"):
+        print(n)
+        self.name = n
+        if n == 'fashion':
+            self.class_names = ['T-shirt', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                   'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+            (self.train_images, self.train_labels), (self.test_images, self.test_labels) = self.load_fashion()
+
+        elif n == 'cifar10':
+            self.class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
+            (self.train_images, self.train_labels), (self.test_images, self.test_labels) = self.load_cifar10()
+
+        else:
+            self.class_names = ['Zero', 'One', 'Two', 'Three', 'Four',
+               'Five', 'Six', 'Seven', 'Eight', 'Nine']
+            (self.train_images, self.train_labels), (self.test_images, self.test_labels) = self.load_numbers()
+
+    def load_fashion(self):
+        fashion_mnist = tf.keras.datasets.fashion_mnist
+        return fashion_mnist.load_data()
+
+    def load_numbers(self):
+        numbers_mnist = tf.keras.datasets.mnist
+        return numbers_mnist.load_data()
+
+    def load_cifar10(self):
+        cifar10_mnist = tf.keras.datasets.cifar10
+        return cifar10_mnist.load_data()
+
+
+class MLP():
+    def __init__(self, mnist, hidden_layers, activation):
+        self.mnist = mnist
+        self.num_layers = len(hidden_layers)
+        self.hidden_layers = hidden_layers
+        self.activation = activation
+        self.model = self.build_model()
+
+
+    def build_model(self):
+        t_s = self.mnist.train_images.shape
+        print("Shape: ", t_s, type(t_s), t_s[1:], len(t_s))
+
+        if len(t_s) > 3:
+            self.mnist.train_images = tf.image.rgb_to_grayscale(self.mnist.train_images)
+            self.mnist.test_images = tf.image.rgb_to_grayscale(self.mnist.test_images)
+
+        layer_list = [tf.keras.layers.Flatten(input_shape=t_s[1:3])]
+        for number_of_neurons in self.hidden_layers:
+            l = tf.keras.layers.Dense(number_of_neurons, self.activation)
+            layer_list.append(l)
+
+        layer_list.append(tf.keras.layers.Dense(10))
+        model = tf.keras.Sequential(layer_list)
+        return model
+
+    def compile_model(self):
+        self.model.compile(optimizer='adam',
+                      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                      metrics=['accuracy'])
+
+    def train_model(self, epochs=10):
+        self.model.fit(self.mnist.train_images, self.mnist.train_labels, epochs=epochs)
+
+    def evaluate_model(self, verbosity=2):
+        test_loss, test_accuracy = self.model.evaluate(self.mnist.test_images, self.mnist.test_labels, verbose=verbosity)
+
+        return test_loss, test_accuracy
+
+    def prediction(self, pckl=False):
+        prediction_model = tf.keras.Sequential([self.model, tf.keras.layers.Softmax()])
+        pred = prediction_model.predict(self.mnist.test_images)
+        if pckl == True:
+            now = datetime.now()
+            fname = self.mnist.name + "MLPPredictions" + now.strftime("%Y%m%d_%H%M%S") + '.pickle'
+            f = open(fname, "wb")
+            pickle.dump(pred, f)
+            f.close()
+
+        return pred
+
+    def confusion_mat(self, pckl=False):
+        cp = []
+        for pred in self.prediction():
+            cp.append(np.argmax(pred))
+
+        conf_pred = np.array(cp)
+
+        confusion = confusion_matrix(self.mnist.test_labels, conf_pred)
+        if pckl == True:
+            now = datetime.now()
+            mname = self.mnist.name + "MLPConfusion" + now.strftime("%Y%m%d_%H%M%S") + '.pickle'
+            f = open(mname, "wb")
+            pickle.dump(confusion, f)
+            f.close()
+
+        return confusion
+
+
+    def save_model(self, fname=""):
+        if fname == "":
+            now = datetime.now()
+            fname = self.mnist.name + "MLP" + now.strftime("%Y%m%d_%H%M%S") + '.hdf5'
+            self.model.save(fname)
+            return fname
+
+        if fname.split('.')[-1] != "hdf5":
+            fname = fname + '.hdf5'
+
+        self.model.save(fname)
+        return fname
+
+    def load_model(self, fname):
+        if fname == "":
+            print("Please provide a filename to load")
+            return
+        else:
+            self.model = tf.keras.models.load_model(fname)
+
+
+
+class CNN():
+    def __init__(self, mnist, hidden_layers, activation):
+        self.mnist = mnist
+        self.num_layers = len(hidden_layers)
+        self.hidden_layers = hidden_layers
+        self.activation = activation
+        self.model = self.build_model()
+
+    def build_model(self):
+        t_s = self.mnist.train_images.shape
+        if len(t_s) < 4:
+            layer_list = [layers.Conv2D(self.hidden_layers[0][0], (self.hidden_layers[0][1], self.hidden_layers[0][1]),
+                                    input_shape=(28, 28, 1), activation=self.activation)]
+            self.mnist.train_images = self.mnist.train_images.reshape((self.mnist.train_images.shape[0], 28, 28, 1))
+            self.mnist.test_images = self.mnist.test_images.reshape((self.mnist.test_images.shape[0], 28, 28, 1))
+        else:
+            layer_list = [layers.Conv2D(self.hidden_layers[0][0], (self.hidden_layers[0][1], self.hidden_layers[0][1]),
+                                    input_shape=(32, 32, 3), activation=self.activation)]
+
+        i=0
+        for filter, kernel in self.hidden_layers:
+            if i==0:
+                i=1
+                continue
+            else:
+                i=1
+                l = layers.Conv2D(filter, (kernel, kernel), activation=self.activation)
+                layer_list.append(l)
+
+        model = tf.keras.models.Sequential(layer_list)
+        model.add(layers.Flatten())
+        model.add(layers.Dense(10))
+        print(model.summary())
+        return model
+
+    def compile_model(self):
+        self.model.compile(optimizer='adam',
+                      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                      metrics=['accuracy'])
+
+    def train_model(self, epochs=10):
+        self.model.fit(self.mnist.train_images, self.mnist.train_labels, epochs=epochs)
+
+    def evaluate_model(self, verbosity=2):
+        test_loss, test_accuracy = self.model.evaluate(self.mnist.test_images, self.mnist.test_labels, verbose=verbosity)
+
+        return test_loss, test_accuracy
+
+    def prediction(self, pckl=False):
+        prediction_model = tf.keras.Sequential([self.model, tf.keras.layers.Softmax()])
+        pred = prediction_model.predict(self.mnist.test_images)
+        if pckl == True:
+            now = datetime.now()
+            fname = self.mnist.name + "CNNPredictions" + now.strftime("%Y%m%d_%H%M%S") + '.pickle'
+            f = open(fname, "wb")
+            pickle.dump(pred, f)
+            f.close()
+
+        return pred
+
+    def confusion_mat(self, pckl=False):
+        cp = []
+        for pred in self.prediction():
+            cp.append(np.argmax(pred))
+
+        conf_pred = np.array(cp)
+
+        confusion = confusion_matrix(self.mnist.test_labels, conf_pred)
+        if pckl == True:
+            now = datetime.now()
+            mname = self.mnist.name + "CNNConfusion" + now.strftime("%Y%m%d_%H%M%S") + '.pickle'
+            f = open(mname, "wb")
+            pickle.dump(confusion, f)
+            f.close()
+
+        return confusion
+
+
+    def save_model(self, fname=""):
+        if fname == "":
+            now = datetime.now()
+            fname = self.mnist.name + "CNN" + now.strftime("%Y%m%d_%H%M%S") + '.hdf5'
+            self.model.save(fname)
+            return fname
+
+        if fname.split('.')[-1] != "hdf5":
+            fname = fname + '.hdf5'
+
+        self.model.save(fname)
+        return fname
+
+    def load_model(self, fname):
+        if fname == "":
+            print("Please provide a filename to load")
+            return
+        else:
+            self.model = tf.keras.models.load_model(fname)
+
+
+
+
+
+
+
+
+
+
